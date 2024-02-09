6b8c4e914c73634d1d9ec5cd056037b521ac8f9c
Lewis Potter <lewis@detection-technologies.com>
2024-02-09 15:53:28 +0000

    planning refs passthrough

--- Components/TaskCard.tsx
+++ Components/TaskCard.tsx
@@ -1,16 +1,18 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Pressable } from 'react-native'
+import React, { Ref, useRef } from 'react'
 import { ITask, editTask } from '../Redux/Slices/taskSlice'
 import { X } from 'react-native-feather'
 import Checkbox from 'expo-checkbox';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../Redux/store';
 import { setSelectedTask } from '../Redux/Slices/selectedTaskSlice';
+import { Swipeable } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
 
 interface ITaskCard {
 	taskDetails: ITask,
 	index: number,
-	onDelete: (taskID: string) => void;
+	onDelete: (taskID: string) => void,
 }
 
 
@@ -21,31 +23,52 @@ const TaskCard = ({taskDetails, onDelete}: ITaskCard) => {
 	const selectedTask = useSelector((state: RootState) => state.selectedTask.value)
 	const theme = useSelector((state: RootState) => state.theme)
 
+	const ref = useRef(null)
+
 	const toggleTaskCompletion = () => {
         if (selectedTask !== null && taskDetails) {
             dispatch(editTask({id: taskDetails.id, updates: {isCompleted: !taskDetails.isCompleted}}))
         }
     }
 
+	const renderRight = () => (
+
+		<View style={{ width: '100%', paddingLeft: 10, paddingRight: 10, flexDirection: 'row-reverse', alignItems: 'center' }}>
+			<Animated.View style={{ flexDirection: 'row-reverse', backgroundColor: '#AE0000', transform: [{ translateX: 0 }] }}>
+				<Text>hello</Text>
+			</Animated.View>
+		</View>
+	)
+
 	return (
-		<TouchableOpacity 
-			style={{ 
-				...styles.taskCard, 
-				backgroundColor: selectedTask === taskDetails.id ?  theme.primarySelected : theme.primaryColor,
-			}}
-			onPress={() => dispatch(setSelectedTask(taskDetails.id))}
+		<Swipeable
+			key={taskDetails.id}
+			ref={ref}
+			renderRightActions={renderRight}
+			overshootRight={true}
 		>
-			<View style={styles.grouper}>
-				<Checkbox 
-					value={taskDetails.isCompleted} 
-					onValueChange={() => {}}
-					color={taskDetails.isCompleted ? theme.primaryColor : '#b3b3b3'}
-				/>
-				<Text style={styles.text}>{taskDetails.name}</Text>
-			</View>
-			
-			<X onPress={() => onDelete}  fontSize='5' color={'#dedede'}/>
-		</TouchableOpacity>
+			<Pressable
+				style={{
+					...styles.taskCard,
+					backgroundColor: selectedTask === taskDetails.id ? theme.primarySelected : theme.primaryColor,
+				}}
+				onPress={() => {
+					dispatch(setSelectedTask(taskDetails.id))}
+				}
+			>
+				<View style={styles.grouper}>
+					<Checkbox
+						value={taskDetails.isCompleted}
+						onValueChange={() => { }}
+						color={taskDetails.isCompleted ? theme.primaryColor : '#b3b3b3'}
+					/>
+					<Text style={styles.text}>{taskDetails.name}</Text>
+				</View>
+
+				<X onPress={() => onDelete} fontSize='5' color={'#dedede'} />
+			</Pressable>
+		</Swipeable>
+		
 	)
 }
 
